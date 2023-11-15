37ba33972807061ebd4aea8358104e3e18bdbb3e
Kvphalien <kaphgrey@gmail.com>
2023-11-15 03:50:56 -0800

    updated

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A beautiful code that passes betty checks
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Hello, Josiah!\n");
+	return (0);
+}
+
